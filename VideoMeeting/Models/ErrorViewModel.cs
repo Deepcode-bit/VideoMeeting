9413eca55e4b0ba147d313c528856452cdb0c3cb
William <1473054623@qq.com>
2021-03-29 20:40:48 +0800

    添加项目文件。

---
+++ VideoMeeting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoMeeting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
